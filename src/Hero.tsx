c32a9c57dece247cbe63c31b50a97107eacdd7ce
Avery Wagar <ajmw.subs@gmail.com>
2021-12-11 02:01:12 -0800

    Buttons

--- src/Hero.tsx
+++ src/Hero.tsx
@@ -3,6 +3,8 @@ import './Hero.css';
 
 import hero from './images/hero.png';
 
+import Button from 'react-bootstrap/Button';
+
 import {Section, Color} from './components/Section/Section';
 import Profile from './components/Profile/Profile';
 
@@ -10,8 +12,16 @@ const Hero = () => {
   return (
     <Section color={Color["Cream"]}>
       <Profile photoPath={hero}>
-        <h1>JOIN THE PANTHEON</h1>
-        <p>Mystery often surrounds the accuracy of mythology, exemplifying gods as perfect idols in our human space, but these idols have a story… an origin…</p>
+        <div className="hero">
+          <Button>Pantheon</Button>
+          <h1>JOIN THE PANTHEON</h1>
+          <p>Mystery often surrounds the accuracy of mythology, exemplifying gods as perfect idols in our human space, but these idols have a story… an origin…</p>
+          <div className="social-buttons">
+            <Button>Discord</Button>
+            <Button>Twitter</Button>
+            <Button>Instagram</Button>
+          </div>
+        </div>
       </Profile>
     </Section>
   );
